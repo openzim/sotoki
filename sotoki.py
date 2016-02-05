bf93b38a723dda68835b414df5ec5df9e096938a
Amirouche BOUBEKKI <amirouche@hypermove.net>
2016-02-05 10:04:17 +0100

    cf. #8 - pep8

--- sotoki.py
+++ sotoki.py
@@ -399,6 +399,7 @@ def process(args):
                     f.write(post)
     print 'offlining finished', uid
 
+
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in xrange(0, len(l), n):
