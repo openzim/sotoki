d29cec557e7bd9d7face2e893508a3f952f413e2
dattaz <taz@dattaz.fr>
2017-04-06 18:49:00 +0200

    Make release of 0.2 version

---
+++ make_release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -u
+if [[ $# -ne 1 ]]
+then
+	echo "You shoud profide version number as argument"
+  exit 1
+fi
+sed -i "s/version='[^']*',/version='$1',/" setup.py
+git add setup.py
+git commit -m "Make release of $1 version"
+git tag -a $1 -m "Make release of $1 version"
+
+#we build
+python setup.py sdist bdist_wheel
+twine upload dist/*
+
+git push -u origin master
