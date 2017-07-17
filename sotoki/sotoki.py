695cc8900e8352441a755e9573b00ba39caf8416
dattaz <taz@dattaz.fr>
2017-07-17 21:32:43 +0200

    Make release of 0.8.2 version

--- sotoki/sotoki.py
+++ sotoki/sotoki.py
@@ -943,7 +943,7 @@ def create_zim(static_folder, zim_path, title, description, lang_input, publishe
 
 def run():
     try:
-        arguments = docopt(__doc__, version='sotoki 0.8.1')
+        arguments = docopt(__doc__, version='sotoki 0.8.2')
     except DocoptExit:
             print(__doc__)
             sys.exit()
