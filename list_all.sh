d0f313f17980ff50c059ff49b9f56427e39c52cb
Kelson <kelson@kiwix.org>
2019-02-16 14:23:37 +0100

    We should get the proper list now

--- list_all.sh
+++ list_all.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #curl -s https://archive.org/details/stackexchange | egrep "stealth download-pill.*7z.*" | grep -v meta | sed -e "s/.*\///" | sed -e "s/\.7z.*//"  | sed -e "s/-.*//" | sort -u
 #curl https://ia800107.us.archive.org/27/items/stackexchange/stackexchange_files.xml | grep "<file" |sed 's/  <file name="//' | sed "s/\.7z.*//"
-curl --silent https://ia800107.us.archive.org/27/items/stackexchange/stackexchange_files.xml | grep "<file" |sed 's/  <file name="\(.*\)\.7z".*$/\1/' |grep -v "stackexchange_" |grep  ".com$"
+curl --silent https://ia800107.us.archive.org/27/items/stackexchange/stackexchange_files.xml | grep "<file name=.*7z" | sed 's/  <file name="\(.*\)\.7z".*$/\1/' | sed 's/-.*$//' | grep -v "meta." | sort -u
