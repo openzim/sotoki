5e50ed591584af3df8013dab6a98604aa9654484
renaud gaudin <rgaudin@gmail.com>
2020-01-16 11:03:12 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pip.req import parse_requirements
 
 setup(
     name='sotoki',
-    version='1.0',
+    version='1.1',
     description="Make zimfile from stackexchange dump",
     long_description=open('README.md').read(),
     author='dattaz',
