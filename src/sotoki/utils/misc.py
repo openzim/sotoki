ae6492ee7d86176e814b7e82ca00ace5dc559fce
renaud gaudin <rgaudin@gmail.com>
2021-07-14 17:55:43 +0000

    Code formatting: disabling pylint cycling-import
    On-purpose use of Cycling imports for the Global context

--- src/sotoki/utils/misc.py
+++ src/sotoki/utils/misc.py
@@ -3,12 +3,10 @@
 # vim: ai ts=4 sts=4 et sw=4 nu
 
 import zlib
-import queue
 import pathlib
 import logging
 import subprocess
 import urllib.parse
-import concurrent.futures as cf
 from typing import Union, Iterable
 
 import psutil
--- src/sotoki/utils/shared.py
+++ src/sotoki/utils/shared.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
+# pylint: disable=cyclic-import
 
 import datetime
 import threading
